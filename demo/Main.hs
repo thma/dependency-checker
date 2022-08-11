7d085a736784001af6c8914107648efc66d65fe1
thma <gH1710>
2022-08-11 22:21:19 +0200

    demo

--- demo/Main.hs
+++ demo/Main.hs
@@ -1,6 +1,24 @@
 module Main where
 
-import Lib
+import DependencyChecker
 
 main :: IO ()
-main = someFunc
+main = do
+  --let allowed = cleanArchitectureCompliantDeps cleanArchitecturePackages
+
+  print $ formatLeftAsErrMsg $ verifyCleanArchitectureDependencies [bogusDependency] 
+
+  print $ formatLeftAsErrMsg $ verifyCleanArchitectureDependencies [goodDependency]
+
+
+goodDependency :: (ModName, [Import])
+goodDependency = (mod, [imp])
+  where
+    mod =  (fromHierarchy ["ExternalInterfaces"],"FileConfigProvider")
+    imp = Import { impMod = (fromHierarchy ["Domain"],"ReservationDomain"), impType = NormalImp }
+
+bogusDependency :: (ModName, [Import])
+bogusDependency = (mod, [imp])
+  where
+    mod =  (fromHierarchy ["Domain"],"ReservationDomain")
+    imp = Import { impMod = (fromHierarchy ["ExternalInterfaces"],"FileConfigProvider"), impType = NormalImp }  
