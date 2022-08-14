26e1f8575c346b8e9b3c77a77c3c5279fc5e13c5
thma <gH1710>
2022-08-14 10:47:54 +0200

    add some tests

--- demo/Main.hs
+++ demo/Main.hs
@@ -4,11 +4,29 @@ import DependencyChecker
 
 main :: IO ()
 main = do
-  --let allowed = cleanArchitectureCompliantDeps cleanArchitecturePackages
 
   print $ formatLeftAsErrMsg $ verifyCleanArchitectureDependencies [bogusDependency] 
 
   print $ formatLeftAsErrMsg $ verifyCleanArchitectureDependencies [goodDependency]
+
+  allImports <- allImportDeclarations "src"
+ -- print allImports
+  print $ formatLeftAsErrMsg $ Left allImports
+  print $ verifyAllDependencies dcPacks dcAllowed allImports
+
+dcPacks = ["", "Control.Monad", "Data", "System", "Graphmod"]
+
+dcAllowed = [("", "Control.Monad"), ("", "Data"), ("", "System"), ("","Graphmod")]
+
+dcDeps :: (ModName, [Import])
+dcDeps = (mod, [cm,de,dl,sd,gu]) 
+  where
+    mod = (fromHierarchy [],"DependencyChecker")
+    cm  = Import {impMod = (fromHierarchy ["Control", "Monad"],"Extra"), impType = NormalImp }
+    de  = Import {impMod = (fromHierarchy ["Data"],"Either"), impType = NormalImp}
+    dl  = Import {impMod = (fromHierarchy ["Data"],"List"), impType = NormalImp}
+    sd  = Import {impMod = (fromHierarchy ["System"],"Directory"), impType = NormalImp}
+    gu  = Import {impMod = (fromHierarchy ["Graphmod"],"Utils"), impType = NormalImp}
 
 
 goodDependency :: (ModName, [Import])
---
+++ test/DependencyCheckerSpec.hs
@@ -0,0 +1,33 @@
+module DependencyCheckerSpec where
+
+import Test.Hspec ( hspec, describe, it, shouldBe, Spec )
+import DependencyChecker
+    ( ModName,
+      ImpType(..),
+      Import(..),
+      fromHierarchy,
+      verifyCleanArchitectureDependencies,
+      formatLeftAsErrMsg,
+      allImportDeclarations )
+
+
+main :: IO ()
+main = hspec spec
+
+spec :: Spec
+spec =
+  describe "The Dependency Checker" $ do
+    it "makes sure all modules comply to the outside-in rule" $ do
+      allImports <- allImportDeclarations "src"
+      formatLeftAsErrMsg (verifyCleanArchitectureDependencies allImports) `shouldBe` Right ()
+    it "finds non-compliant import declarations" $ do
+      verifyCleanArchitectureDependencies [bogusDependency] `shouldBe`
+        Left [bogusDependency]
+
+-- | this instance represents a non-compliant dependency from the 'Domain' package to the 'ExternalInterfaces' package.
+bogusDependency :: (ModName, [Import])
+bogusDependency = (mod, [imp])
+  where
+    mod =  (fromHierarchy ["Domain"],"ReservationDomain")
+    imp = Import { impMod = (fromHierarchy ["ExternalInterfaces"],"FileConfigProvider"), impType = NormalImp }
+
